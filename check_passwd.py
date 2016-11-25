3a568f7b834ffef7f42388badbd3603d166fa497
Rileyoung <hnymeng@163.com>
2016-11-25 10:38:04 +0800

    check_passwd
    check weak passwd

---
+++ check_passwd.py
@@ -0,0 +1,48 @@
+#-*- coding: utf-8 _*_
+#!/usr/bin/python
+import re, os, sys
+import ConfigParser
+import string
+import paramiko
+
+cf=ConfigParser.ConfigParser()
+cf.read("test.conf")
+port = cf.getint("hostn", "db_port")
+username = cf.get("hostn","db_user")
+
+host_file=open("host.txt","r")
+host_lines=host_file.readlines()
+host_counter=0
+
+password_file=open("passwd.txt","r")
+password_lines = password_file.readlines()
+
+print "\n\t......starting......"
+
+for host_line in host_lines:
+    password_counter=0
+    host_counter=host_counter+1
+    host_line=host_line.strip()
+    #password=line
+    hostname=host_line
+    print "\n\t Check IP：\t %s\n"%(hostname)
+    for password_line in password_lines:
+        password_counter=password_counter+1
+        password_line=password_line.strip()
+        password=password_line
+
+        client = paramiko.SSHClient()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        try:
+            client.connect(hostname,port,username,password)
+            stdin, stdout, stderr = client.exec_command("pwd")
+            #print stdout.read()
+            print "password check [%d] : Success!\tweak passwd is：%s"%(password_counter,password)
+            break
+        except:
+            print "password check [%d] : Failed!"%(password_counter)
+        client.close()
+password_file.close()
+host_file.close()
+
+print "\n\t......ending......\n"
